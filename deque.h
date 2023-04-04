5647a36958a7480c80962aafb0eed2616b2495a9
Aleksandr Alekseev <aalekseevx@protonmail.com>
2023-04-04 11:39:33 +0300

    Initial commit

---
+++ deque.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <deque>
+
+template <typename T>
+using Deque = std::deque<T>;
+
+// TODO: Solution above is smart enough, but please, remove it and add your declarations here
